8590d054daa61a2546e5d34eb6b5d23451657bcb
breabandaniel <brbndan@gmail.com>
2026-04-10 18:03:15 +0300

    added .sql file

---
+++ socially.sql
@@ -0,0 +1,160 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema socially
+-- -----------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS `socially` DEFAULT CHARACTER SET utf8 ;
+USE `socially` ;
+
+-- -----------------------------------------------------
+-- Table `socially`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`outgoings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`outgoings` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) CHARACTER SET 'utf8mb4' NOT NULL,
+  `url` VARCHAR(2048) CHARACTER SET 'utf8mb4' NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `socially`.`filters`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`filters` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`user_filters`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`user_filters` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `filter_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_filter_id_idx` (`filter_id` ASC) VISIBLE,
+  INDEX `fk_user_id_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_filter_id`
+    FOREIGN KEY (`filter_id`)
+    REFERENCES `socially`.`filters` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `socially`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`locations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`locations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`location_filters`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`location_filters` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `location_id` INT NOT NULL,
+  `filter_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_loc_filt_location_idx` (`location_id` ASC) VISIBLE,
+  INDEX `fk_loc_filt_filter_idx` (`filter_id` ASC) VISIBLE,
+  CONSTRAINT `fk_loc_filt_location`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `socially`.`locations` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_loc_filt_filter`
+    FOREIGN KEY (`filter_id`)
+    REFERENCES `socially`.`filters` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`outgoing_locations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`outgoing_locations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `outgoing_id` INT NOT NULL,
+  `location_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_out_loc_outgoing_idx` (`outgoing_id` ASC) VISIBLE,
+  INDEX `fk_out_loc_location_idx` (`location_id` ASC) VISIBLE,
+  CONSTRAINT `fk_out_loc_outgoing`
+    FOREIGN KEY (`outgoing_id`)
+    REFERENCES `socially`.`outgoings` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_out_loc_location`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `socially`.`locations` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`groups`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`groups` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `socially`.`group_users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `socially`.`group_users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `group_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_gu_group_idx` (`group_id` ASC) VISIBLE,
+  INDEX `fk_gu_user_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_gu_group`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `socially`.`groups` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_gu_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `socially`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
