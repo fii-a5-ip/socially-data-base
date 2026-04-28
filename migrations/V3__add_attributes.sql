90227c84302eed55936a4b4249b93409252362a1
breabandaniel <brbndan@gmail.com>
2026-04-28 19:54:29 +0300

    flyway repair

--- migrations/V3__add_attributes.sql
+++ migrations/V3__add_attributes.sql
@@ -1,9 +1,9 @@
 ALTER TABLE `socially`.`users` 
-ADD COLUMN `fullname` VARCHAR(45) NOT NULL AFTER `username`,
-ADD COLUMN `email` VARCHAR(45) NULL AFTER `fullname`,
-ADD COLUMN `password` VARCHAR(45) NULL AFTER `email`,
-ADD COLUMN `google_id` VARCHAR(45) NULL AFTER `password`,
-ADD COLUMN `profile_img_url` VARCHAR(2048) NULL AFTER `google_id`;
+ADD COLUMN `fullname` VARCHAR(45) NOT NULL,
+ADD COLUMN `email` VARCHAR(45) NULL,
+ADD COLUMN `password` VARCHAR(45) NULL,
+ADD COLUMN `google_id` VARCHAR(45) NULL,
+ADD COLUMN `profile_img_url` VARCHAR(2048) NULL;
 
 ALTER TABLE `socially`.`users` 
 ADD UNIQUE INDEX `fullname_UNIQUE` (`fullname` ASC) VISIBLE,
@@ -11,14 +11,14 @@ ADD UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
 ADD UNIQUE INDEX `google_id_UNIQUE` (`google_id` ASC) VISIBLE;
 
 ALTER TABLE `socially`.`locations` 
-ADD COLUMN `latitude` DECIMAL(10,8) NOT NULL AFTER `name`,
-ADD COLUMN `longitude` DECIMAL(10,8) NOT NULL AFTER `latitude`,
-ADD COLUMN `img_url` VARCHAR(2048) NULL AFTER `longitude`;
+ADD COLUMN `latitude` DECIMAL(10,8) NOT NULL,
+ADD COLUMN `longitude` DECIMAL(10,8) NOT NULL,
+ADD COLUMN `img_url` VARCHAR(2048) NULL;
 
 ALTER TABLE `socially`.`events` 
-ADD COLUMN `creator_user_id` INT NOT NULL AFTER `location_id`,
-ADD COLUMN `desc` LONGTEXT NOT NULL AFTER `creator_user_id`,
-ADD COLUMN `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `desc`;
+ADD COLUMN `creator_user_id` INT NOT NULL,
+ADD COLUMN `desc` LONGTEXT NOT NULL,
+ADD COLUMN `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 ALTER TABLE `socially`.`events` 
 ADD INDEX `fk_events_creator_idx` (`creator_user_id` ASC) VISIBLE,
@@ -29,8 +29,8 @@ ADD CONSTRAINT `fk_events_creator`
   ON UPDATE CASCADE;
 
 ALTER TABLE `socially`.`groups` 
-ADD COLUMN `creator_user_id` INT NOT NULL AFTER `name`,
-ADD COLUMN `img_link` VARCHAR(2048) NULL AFTER `creator_user_id`;
+ADD COLUMN `creator_user_id` INT NOT NULL,
+ADD COLUMN `img_link` VARCHAR(2048) NULL;
 
 ALTER TABLE `socially`.`groups` 
 ADD INDEX `fk_groups_creator_idx` (`creator_user_id` ASC) VISIBLE,
